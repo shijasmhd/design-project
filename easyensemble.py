c6b96302dbd2972e6e59dede9acd07480b75ca4b
robyjacob <robyjacob1998@gmail.com>
2018-10-07 23:27:10 +0530

    added easyensemble

---
+++ easyensemble.py
@@ -0,0 +1,19 @@
+from imblearn.ensemble import EasyEnsemble
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+from sklearn.datasets import make_classification
+
+X, y = make_classification(n_samples=5000, n_features=2, n_informative=2,
+                           n_redundant=0, n_repeated=0, n_classes=3,
+                           n_clusters_per_class=1,
+                           weights=[0.01, 0.05, 0.94],
+                           class_sep=0.8, random_state=0)
+
+e = EasyEnsemble(random_state=0, n_subsets=10)
+e.fit(X, y)
+X_resampled, y_resampled = e.sample(X, y)
+colors = ['#ef8a62' if v == 0 else '#f7f7f7' if v == 1 else '#67a9cf' for v in y_resampled[0, :]]
+plt.scatter(X_resampled[0, :, 0], X_resampled[0, :, 1], c=colors, linewidth=1, edgecolor='black')
+plt.show()
+sns.despine()
