9dc5663d4e65621aab1e664b3347dc3516201a90
shijasmhd <43935772+shijasmhd@users.noreply.github.com>
2018-10-07 23:28:39 +0530

    Update easyensemble.py

--- easyensemble.py
+++ easyensemble.py
@@ -4,6 +4,7 @@ import numpy as np
 import seaborn as sns
 from sklearn.datasets import make_classification
 
+
 X, y = make_classification(n_samples=5000, n_features=2, n_informative=2,
                            n_redundant=0, n_repeated=0, n_classes=3,
                            n_clusters_per_class=1,
